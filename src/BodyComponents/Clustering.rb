3f1e420da7427088fe01d439734417005ebd05b4
Bjoern Rennhak <bjoern@rennhak.com>
2010-11-04 20:05:17 +0900

    - Added clustering stub

---
+++ src/BodyComponents/Clustering.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+#
+
+###
+#
+# File: Clustering.rb
+#
+######
+
+
+###
+#
+# (c) 2009-2011, Copyright, Bjoern Rennhak, The University of Tokyo
+#
+# @file       Clustering.rb
+# @author     Bjoern Rennhak
+#
+#######
+
+
+require 'k_means'
+
+class Clustering # {{{
+
+  # Constructor
+  def initialize options # {{{
+    @options = options
+  end # of def initialize }}}
+
+
+
+end # of class Clustering }}}
+
+
+# Direct Invocation
+if __FILE__ == $0 # {{{
+
+end # of if __FILE__ == $0 }}}
