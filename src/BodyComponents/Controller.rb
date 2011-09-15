a2b6894af1072ae56e8df606f4274fb176d0dd0f
Bjoern Rennhak <github@thewebagency.org>
2011-09-15 22:01:30 +0900

    - Major refactoring of config dir and config loading. Introduced several new cli switches for cycle, name, speed, etc.

--- src/BodyComponents/Controller.rb
+++ src/BodyComponents/Controller.rb
@@ -76,6 +76,31 @@ class Controller # {{{
     @config.cache_dir             = "cache"
 
     # Determine which configs are available
+    @motions                      = OpenStruct.new  # store all results here for list etc.
+
+    @names                        = []
+    @patterns                     = []
+    @speeds                       = []
+    @cycles                       = []
+
+    # Store all components here
+    @yamls                                                                = Dir.glob( File.join( "**", "*.yaml" ) ).collect { |i| i.split( "/" ) }
+    @yamls.each do |configurations_dir, name, pattern, speed, cycle, filename|
+
+      eval( "@motions.#{name}                                             = OpenStruct.new if( @motions.#{name}.nil? )" )
+      eval( "@motions.#{name}.#{pattern}                                  = OpenStruct.new if( @motions.#{name}.#{pattern}.nil? )" )
+      eval( "@motions.#{name}.#{pattern}.speed_#{speed}                   = OpenStruct.new if( @motions.#{name}.#{pattern}.speed_#{speed}.nil? )" )
+      eval( "@motions.#{name}.#{pattern}.speed_#{speed}.cycle_#{cycle}    = OpenStruct.new if( @motions.#{name}.#{pattern}.speed_#{speed}.cycle_#{cycle}.nil? )" )
+
+      eval( "@motions.#{name}.#{pattern}.speed_#{speed}.cycle_#{cycle}.filename     = '#{filename.to_s}'" )
+      eval( "@motions.#{name}.#{pattern}.speed_#{speed}.cycle_#{cycle}.path         = '#{configurations_dir}/#{name}/#{pattern}/#{speed}/#{cycle}'" )
+
+      @names      << name       unless( @names.include?(    name       ) )
+      @patterns   << pattern    unless( @patterns.include?( pattern    ) )
+      @speeds     << speed      unless( @speeds.include?(   speed      ) )
+      @cycles     << cycle      unless( @cycles.include?(   cycle      ) )
+    end
+
     @configurations               = Dir[ "#{@config.config_dir}/*.yaml" ].collect { |d| d.gsub( "#{@config.config_dir}/", "" ).gsub( ".yaml", "" ) }
     @sides                        = %w[left right both]
     @body_parts                   = %w[hands fore_arms upper_arms thighs shanks feet]
@@ -94,61 +119,69 @@ class Controller # {{{
       # Reuse if desired
       use_cache     if( @options.cache )
 
-      unless( @options.process.empty? )
-        @options.process.each do |motion_config_file|
-          motion_config_filename    = @config.config_dir + "/" + motion_config_file + ".yaml"
-          @log.message :info, "Loading Motion Capture config file (#{motion_config_filename})"
+      @log.message :error, "No processing name given via --name '#{@options.process}'" if( @options.process == "" )
 
-          @motion_config            = read_motion_config( motion_config_filename )
+      unless( @options.process == "" )
 
-          @file                     = @motion_config.filename
-          @from                     = @motion_config.from
-          @to                       = @motion_config.to
-          @name                     = @motion_config.name
-          @dmps                     = @motion_config.dmp
+        %w[name pattern speed cycle].each { |i| raise ArgumentError, "You didn't provide a #{i} via CLI!" if( eval( "@options.#{i} == \"\"" ) ) }
+        @log.message :success, "Using #{@options.process} with pattern #{@options.pattern}, speed #{@options.speed} and cycle #{@options.cycle}"
+        motion_config             = eval( "@motions.#{@options.process}.#{@options.pattern}.speed_#{@options.speed}.cycle_#{@options.cycle}" )
 
-          @dance_master_poses       = []
-          @dance_master_poses_range = []
-          @dmps.each { |dmp_array| @dance_master_poses << dmp_array.first; @dance_master_poses_range << dmp_array.last }
+        raise ArgumentError, "The configuration and/or the data you requested doesn't exist!" if( motion_config.nil? )
 
-          @log.message :info, "Loading the Motion Capture data (#{@file}) via the MotionX VPM Plugin"
-          @adt                      = ADT.new( @file )
+        motion_config_filename    = motion_config.path + "/" + motion_config.filename
+        
+        @log.message :info, "Loading Motion Capture config file (#{motion_config_filename})"
+
+        @motion_config            = read_motion_config( motion_config_filename )
+
+        @file                     = @motion_config.filename
+        @from                     = @motion_config.from
+        @to                       = @motion_config.to
+        @name                     = @motion_config.name
+        @dmps                     = @motion_config.dmp
+
+        @dance_master_poses       = []
+        @dance_master_poses_range = []
+        @dmps.each { |dmp_array| @dance_master_poses << dmp_array.first; @dance_master_poses_range << dmp_array.last }
+
+        @log.message :info, "Loading the Motion Capture data (#{@file}) via the MotionX VPM Plugin"
+        @adt                      = ADT.new( @file )
 
 
-          # o = Marshal.dump( @adt )
+        # o = Marshal.dump( @adt )
 
-          if( @options.filter_motion_capture_data )
-            @log.message :info, "Filter Motion Capture data to smooth out outliers"
-            @filter                 = Filter.new( @options, @from, @to )
-            @adt                    = @filter.filter_motion_capture_data( @adt )
-          end
+        if( @options.filter_motion_capture_data )
+          @log.message :info, "Filter Motion Capture data to smooth out outliers"
+          @filter                 = Filter.new( @options, @from, @to )
+          @adt                    = @filter.filter_motion_capture_data( @adt )
+        end
 
-          if( @options.turning_pose_extraction )
-            @log.message :info, "Performing CPA-PCA Turning pose extraction"
-            @turning                = Turning.new( @options, @adt, @dance_master_poses, @dance_master_poses_range, @from, @to )
-            @turning.get_data
-          end
+        if( @options.turning_pose_extraction )
+          @log.message :info, "Performing CPA-PCA Turning pose extraction"
+          @turning                = Turning.new( @options, @adt, @dance_master_poses, @dance_master_poses_range, @from, @to )
+          @turning.get_data
+        end
 
-          ## b0rked! Singleton methods - but where??! (?? http://doc.okkez.net/191/view/method/Object/i/initialize_copy )
-          ## Speedup by loading a Marshalled object from /tmp/ if previously run
-          #if File.exist?( "/tmp/Controller_Marshall_VPM_Data.tmp" )
-          #  puts "Loading Marshal dump I found in /tmp/Controller_Marshall_VPM_Data.tmp for speedup"
-          #  vpm = Marshal.load( File.read("/tmp/Controller_Marshall_VPM_Data.tmp").readlines.to_s )
-          #else
-          #  vpm   = ADT.new( file )
-          #  x = vpm.deep_clone.to_s
+        ## b0rked! Singleton methods - but where??! (?? http://doc.okkez.net/191/view/method/Object/i/initialize_copy )
+        ## Speedup by loading a Marshalled object from /tmp/ if previously run
+        #if File.exist?( "/tmp/Controller_Marshall_VPM_Data.tmp" )
+        #  puts "Loading Marshal dump I found in /tmp/Controller_Marshall_VPM_Data.tmp for speedup"
+        #  vpm = Marshal.load( File.read("/tmp/Controller_Marshall_VPM_Data.tmp").readlines.to_s )
+        #else
+        #  vpm   = ADT.new( file )
+        #  x = vpm.deep_clone.to_s
 
-          #  puts "Creating Marshal dump for later speedups (/tmp/Controller_Marshall_VPM_Data.tmp)"
-          #  o = Marshal.dump( x )
-          #  File.open( "/tmp/Controller_Marshall_VPM_Data.tmp", File::CREAT|File::TRUNC|File::RDWR, 0644) { |f| f.write( o.to_s ) }
-          #end
+        #  puts "Creating Marshal dump for later speedups (/tmp/Controller_Marshall_VPM_Data.tmp)"
+        #  o = Marshal.dump( x )
+        #  File.open( "/tmp/Controller_Marshall_VPM_Data.tmp", File::CREAT|File::TRUNC|File::RDWR, 0644) { |f| f.write( o.to_s ) }
+        #end
 
-          # p vpm.segments
-          # See shiratori thesis page 132
-          #
+        # p vpm.segments
+        # See shiratori thesis page 132
+        #
 
-          @log.message :success, "Finished processing of #{motion_config_filename.to_s}"
-        end # of @options.process.each
+        @log.message :success, "Finished processing of #{motion_config_filename.to_s}"
       end # of unless( @options.process.empty? )
 
 
@@ -186,7 +219,7 @@ class Controller # {{{
     # Define default options
     options.verbose                         = false
     options.colorize                        = false
-    options.process                         = []
+    options.process                         = ""
     options.turning_pose_extraction         = false
     options.filter_motion_capture_data      = false
     options.boxcar_filter                   = nil
@@ -198,7 +231,9 @@ class Controller # {{{
     options.profiling                       = false
     options.model                           = 12
     options.side                            = "both"
-    options.cycle                           = 1
+    options.cycle                           = ""
+    options.pattern                         = ""
+    options.speed                           = ""
 
     pristine_options                        = options.dup
 
@@ -225,11 +260,11 @@ class Controller # {{{
         options.boxcar_filter = b
       end
 
-      opts.on("-p", "--parts OPT", @body_parts, "Proces one or more body parts during the computation (OPT: #{@body_parts.join(", ")})" ) do |p|
+      opts.on("-p", "--parts OPT", @body_parts, "Proces one or more body parts during the computation (OPT: #{@body_parts.sort.join(", ")})" ) do |p|
         options.body_parts << p
       end
 
-      opts.on("-s", "--side OPT", @sides, "Choose which side to process (OPT: #{@sides.join(", ")}) - Default: #{options.side.to_s}" ) do |s|
+      opts.on("-o", "--orientation OPT", @sides, "Choose which side to process (OPT: #{@sides.sort.join(", ")}) - Default: #{options.side.to_s}" ) do |s|
         options.side = s
       end
 
@@ -237,12 +272,16 @@ class Controller # {{{
         options.model = m
       end
 
-      opts.on("-z", "--cycle NUM", "Determine which dance cycle to use (default: 1)" ) do |z|
+      opts.on("-p", "--pattern OPT", @patterns, "Determine which pattern the dance motions have from cycle to cycle (e.g. #{@patterns.sort.join(", ")})" ) do |p|
+        options.pattern = p
+      end
+
+      opts.on("-z", "--cycle NUM", @cycles, "Determine which dance cycle to use (e.g. #{@cycles.sort.join(", ")} - Default: 001)" ) do |z|
         options.cycle = z
       end
 
-      opts.on("-s", "--speed NUM", "Determine which speed to use (default: 100)" ) do |z|
-        options.cycle = z
+      opts.on("-s", "--speed NUM", @speeds, "Determine which speed to use (#{@speeds.sort.join(", ")} - Default: 100)" ) do |z|
+        options.speed = z
       end
 
       opts.on("-r", "--raw-data", "Use raw data for PCA reduction instead of CPA data") do |r|
@@ -253,24 +292,23 @@ class Controller # {{{
       opts.separator "Specific options:"
 
       # Set of arguments
-      opts.on("-p", "--process OPT", @configurations, "Process one or more detected configuration (OPT: #{ @configurations.sort.join(', ') })" ) do |d|
-        options.process << d
+      opts.on("-n", "--name OPT", @names, "Name of motion capture data to be processed (OPT: #{ @names.sort.join(', ') })" ) do |d|
+        options.process = d
       end
 
-      # Boolean switch.
-      opts.on("-v", "--verbose", "Run verbosely") do |v|
-        options.verbose = v
+      opts.on("-l", "--list", "List avaialble motion capture data with pattern, cycles, speed, etc." )  do |l| 
+        options.list        = l
+        puts "\nYou can choose from the following configuration files\n\n"
+        @yamls.each do |configurations_dir, name, pattern, speed, cycle, filename|
+          printf( "Name (--name): %-20s  Pattern (--pattern): %-10s  Speed (--speed): %-4s   Cycle (--cycle): %-4s \n", name, pattern, speed, cycle )
+        end
+        puts "\n"
+        exit
       end
 
-      # Boolean switch.
-      opts.on("-q", "--quiet", "Run quietly, don't output much") do |v|
-        options.verbose = v
-      end
-
-      # Boolean switch.
-      opts.on("--profiler", "Run profiler alongside the code (see results in tmp/)") do |p|
-        options.profiling = p
-      end
+      opts.on("-v", "--verbose", "Run verbosely")                                                       { |v| options.verbose     = v           }
+      opts.on("-q", "--quiet", "Run quietly, don't output much")                                        { |v| options.quiet       = q           }
+      opts.on("--profiler", "Run profiler alongside the code (see results in tmp/)")                    { |p| options.profiling   = p           }
 
 
       opts.separator ""
